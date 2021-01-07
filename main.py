8d0b59a1cebeb806f29f662bd8128181f4ff493a
yangjiarui <245837941@qq.com>
2021-01-07 21:17:52 +0800

    0.3 version python 3.8

--- main.py
+++ main.py
@@ -8,9 +8,9 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
-    print "hello"
-    print "whoareyou1998"
-    print "test"
+    print("hello")
+    print("whoareyou1998")
+    print("test")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
