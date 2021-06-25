6c6510522713beed2940e3dbde7a6f67573bd02d
yangjiarui <245837941@qq.com>
2021-06-25 18:59:57 +0800

    L-1.0 version learning python 3.8 6.25.2021

--- main.py
+++ main.py
@@ -5,18 +5,5 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-msg="helloworld"
-print(msg)
-msg="initialproject"
-print(msg)
+user_name = "Yjr"
+print(user_name.upper())
